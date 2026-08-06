f099cfa4e928608964193df638cb5dc9e838c303
unknown <antuvhai@gmail.com>
2026-08-06 11:08:41 +0600

    added question and answer

---
+++ question.four.js
@@ -0,0 +1,3 @@
+const question ="What is the difference between == and === in JavaScript?"
+
+const answer ="জাভাস্ক্রিপ্টে (JavaScript) == (Loose Equality) এবং === (Strict Equality) অপারেটর দুটির মূল পার্থক্য হলো ডেটা টাইপ চেক করার নিয়মে।সহজ কথায়: == শুধুমাত্র ভ্যারিয়েবলের মান (Value) চেক করে, কিন্তু === একই সাথে ভ্যারিয়েবলের মান (Value) এবং ডেটা টাইপ (Data Type) দুটিই চেক করে।"
---
+++ question.three.js
@@ -0,0 +1,3 @@
+const question = "What are the primitive data types in JavaScript?";
+
+const answer = "জাভাস্ক্রিপ্টে প্রিমিটিভ ডেটা টাইপ মোট ৭টি: Number, String, Boolean, Undefined, Null, Symbol, এবং BigInt। এদের মূল বৈশিষ্ট্য হলো এরা Immutable (এদের সরাসরি মডিফাই করা যায় না) এবং এরা মেমরিতে রেফারেন্স হিসেবে না থেকে সরাসরি ভ্যালু বা মান হিসেবে স্টোর হয়। এর বাইরে অন্য সব ডেটা টাইপ (যেমন- Array, Function) মূলত Object বা নন-প্রিমিটিভ টাইপের অন্তর্ভুক্ত।";
